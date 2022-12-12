6c7c03f1705516988f63ccb6407f4c7b4bfa3742
rohinraj <rohinsandro@gmail.com>
2022-12-12 23:15:40 +0530

    Q8

---
+++ 8assignment.py
@@ -0,0 +1,7 @@
+#Write a python program to input a list of 'n' numbers.Calculate and display the sum of cubes each value in the list. ( university question) 
+lst1 = []
+lst1 = [int(item) for item in input("Enter the list items seperated by Spaces : ").split()]
+res = 0
+for i in lst1:
+    res += i*i*i
+print("sum of cubes of each value in the list: "+str(res))
