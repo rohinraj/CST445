086b2dee8a970ffa7efc65f4f87bbb914b657ef8
rohinraj <rohinsandro@gmail.com>
2022-12-12 22:37:44 +0530

    Q5

---
+++ 5assignment.py
@@ -0,0 +1,7 @@
+import string
+S = str(input("Enter The Sentence: "))
+noPunctuation = ""
+for c in S:
+    if c not in string.punctuation:
+        noPunctuation = noPunctuation + c
+print("String Without Punctuation: " + noPunctuation)
